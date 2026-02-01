61fac42f6983831e1b818baaf96bd939512122c6
Nathaniel <restofnate@gmail.com>
2026-01-31 22:21:37 -0500

    Created Arm Presets for arm positions

--- src/main/java/frc/robot/commands/ArmCommands.java
+++ src/main/java/frc/robot/commands/ArmCommands.java
@@ -1,15 +1,21 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.position_joint.PositionJoint;
 import frc.robot.util.mechanical_advantage.LoggedTunableNumber;
 
 public class ArmCommands {
 
   public static final class ARM_PRESETS {
-
-    public static final LoggedTunableNumber ARM_STOWED =
-        new LoggedTunableNumber("ArmPresets/ArmStowed", 0.0);
-
-    public static final LoggedTunableNumber ARM_SHOOT =
-        new LoggedTunableNumber("ArmPresets/ArmShoot", 60.0);
+    public static final LoggedTunableNumber STOW = new LoggedTunableNumber("ArmPresets/Stow", 0.0);
+    public static final LoggedTunableNumber FERRY = new LoggedTunableNumber("ArmPresets/Ferry", 45.0);
+    public static final LoggedTunableNumber SHOOT = new LoggedTunableNumber("ArmPresets/Shoot", 90.0);
   }
+
+  /** Command factory to set the arm to a specific preset. */
+  public static Command setSetpoint(PositionJoint arm, LoggedTunableNumber preset) {
+    return PositionJoint.setPosition(arm, preset::get);
+  }
+
+  private ArmCommands() {}
 }
